77b7f1ac28f15cb003c95cffec71064afa2903a0
Berillo Anton <anberg137@gmail.com>
2021-11-11 20:49:09 +0700

    change num_5

--- num_5.sh
+++ num_5.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-fname=$(git grep -l 'func globalPluginDirs(') && git log -L :globalPluginDirs:$fname | grep 'commit' -A 4
-#git log -S'func providerSource(' --oneline
+git log -S'func providerSource(' --oneline
